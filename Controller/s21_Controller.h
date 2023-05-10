65f9b07b02ddd0fe290a2fa8f9bc33eee16d7276
Grandpa Trula <grandpat@un-l5.kzn.21-school.ru>
2023-05-10 20:39:40 +0300

    push

--- Controller/s21_Controller.h
+++ Controller/s21_Controller.h
@@ -1,16 +1,17 @@
 #ifndef SRC_CONTROLLER_SRC_H_
 #define SRC_CONTROLLER_SRC_H_
 #include "../parsing/s21_viewer.h"
+#include <vector>
 
 class Controller {
   s21::Model model_;
   s21::Model::data_t obj;
   public:
   using unit = unsigned int;
-  unit get_count_vertex {};
-  unit get_count_facets {};
-  long double get_arr_vertex {};
-  unit get_arr_facets {};
+  unit get_count_vertex() {return obj.count_vert;}
+  unit get_count_facets() {return obj.count_facets;}
+  double *get_arr_vertex() { return obj.vertexes;}
+  unit *get_arr_facets() {return obj.facets;}
   bool set_find_file(std::string str) {
     return model_.s21_count_v_f(str, &obj);
   }
