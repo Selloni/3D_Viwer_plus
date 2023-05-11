d3eb0ea2b16962888e19b723573d318f7e157dbc
Grandpa Trula <grandpat@un-l5.kzn.21-school.ru>
2023-05-11 16:37:11 +0300

    facade

--- Facade/s21_facade.h
+++ Facade/s21_facade.h
@@ -2,14 +2,16 @@
 #define SRC_FACADE_H_
 
 #include "../Controller/s21_controller.h"
-// #include "../3DWieve/mainwindow.h"
+#include "../3DWieve/mainwindow.h"
 
 namespace s21 {
 class Facade {
   Controller controller_;
-  // MainWindow MainWindow = new MainWindow();
+  MainWindow maintwindow_;
+
   public:
   bool set_path(std::string);
+  void get_value(unit &qcount_vert, unit &qcount_facets, double &qvertexes, unit &qfacets);
 
 }; // Facade
 } // s21
